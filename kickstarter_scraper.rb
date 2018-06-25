8e1abd1369594a5a57bea4b8ce8bf8244e1b3db7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-25 14:17:51 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ kickstarter = Nokogiri::HTML(html)
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").textproject.css
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description : project.css("p.bbcard_blurb").text
 
 def create_project_hash
   # write your code here
