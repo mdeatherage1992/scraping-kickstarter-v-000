f177cc069378de0c269411a09094c9622bcb3820
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-25 14:13:48 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,8 @@ html = File.read('fixtures/kickstarter.html')
 
 kickstarter = Nokogiri::HTML(html)
 
+# projects: kickstarter.css("li.project.grid_4")
+
 def create_project_hash
   # write your code here
 end
